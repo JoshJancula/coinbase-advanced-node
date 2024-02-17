3dc21e9dbbe616b3b406ad04af6b83b76de6c241
Joshua Jancula <josh@jancula.com>
2024-02-17 14:30:53 -0500

    doc: update demo script

--- src/demo/init-client.ts
+++ src/demo/init-client.ts
@@ -2,7 +2,7 @@ import {Coinbase} from '..';
 import 'dotenv-defaults/config';
 
 export function initClient(): Coinbase {
-  if (process.env.COINBASE_API_KEY !== '') {
+  if (process.env.COINBASE_API_KEY !== '' || process.env.CLOUD_API_KEY_NAME !== '') {
     return new Coinbase({
       apiKey: process.env.COINBASE_API_KEY!,
       apiSecret: process.env.COINBASE_API_SECRET!,
